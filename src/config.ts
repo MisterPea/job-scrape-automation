2d960906a878011c5f8938f3aad0d4836fdc07b1
Perry Angelora <pea.mail.list@gmail.com>
2025-12-03 17:21:18 -0500

    Added ability to add a block list as an array of string urls: i.e. `["facebook.com/groups","careers.cognizant.com"]`

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,17 @@ export type AppConfig = {
   sortBy: string;
   language: Language;
   searchCountry: SearchCountry;
+  blockList: string[];
 };
 
 export function loadConfig(): AppConfig {
   if (!process.env.SEARCH_API_KEY) throw new Error("SEARCH_API_KEY is required");
   if (!process.env.SITE_SEARCH_ID_CX) throw new Error("SITE_SEARCH_ID_CX is required");
+
+  const blockList = [
+    'facebook.com/groups',
+    'careers.cognizant.com'
+  ];
 
   return {
     searchApiKey: process.env.SEARCH_API_KEY,
@@ -20,6 +26,7 @@ export function loadConfig(): AppConfig {
     timePeriod: 'd1',
     sortBy: 'date',
     language: 'lang_en',
-    searchCountry: 'us'
+    searchCountry: 'us',
+    blockList,
   };
 }
